787ba1ac6b50e79be9731e3799399d06a07f3c7b
crccheck <c@crccheck.com>
2014-06-22 14:22:58 -0500

    sketch out initial models

--- ox_scale/apps/scale/models.py
+++ ox_scale/apps/scale/models.py
@@ -1,3 +1,56 @@
+from django.conf import settings
 from django.db import models
+from django_extensions.db.fields import json
 
-# Create your models here.
+
+class Dataset(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Datapoint(models.Model):
+    dataset = models.ForeignKey(Dataset, related_name='datapoints')
+
+    name = models.CharField(max_length=255, unique=True)
+    meta = json.JSONField()
+
+    # match
+    match = models.ForeignKey('self', related_name='+', null=True, blank=True)
+    match_confidence = models.FloatField(null=True, blank=True,
+        default='How confident are we that this match is right?')
+    match_won = models.PositiveIntegerField(default=0,
+        help_text='How many times the match was chosen')
+    match_sample = models.PositiveIntegerField(default=0,
+        help_text='How many times this question was asked')
+
+    # bookkeeping fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_from = models.CharField(max_length=100)
+
+
+class Question(models.Model):
+    """
+    Asks a question about a datapoint in a dataset.
+    """
+    datapoint = models.OneToOneField(Datapoint, null=True, blank=True,
+        related_name='question')
+    includes = models.CommaSeparatedIntegerField(max_length=100,
+        help_text='These are PKs of the `Datapoint`s the '
+        'question should prefer to offer as choices.')
+    excludes = models.CommaSeparatedIntegerField(
+        max_length=255,
+        help_text='These `Datapoint` PKs should be excluded '
+        'as answer choices.')
+
+
+class Response(models.Model):
+    """
+    Every response to a `Question`.
+    """
+    question = models.ForeignKey(Question)
+    choice = models.ForeignKey(Datapoint)
+
+    # bookkeeping
+    created_at = models.DateTimeField(auto_now_add=True)
+    ip_address = models.CharField(max_length=48)
+    user_agent = models.CharField(max_length=200)
