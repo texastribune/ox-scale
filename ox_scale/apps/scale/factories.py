76b5f91aca732ffcb93f1bb759e15e7793381327
crccheck <c@crccheck.com>
2014-06-22 11:57:30 -0500

    hey, first app

---
+++ ox_scale/apps/scale/factories.py
@@ -0,0 +1,3 @@
+import factory
+
+from . import models
--- ox_scale/settings.py
+++ ox_scale/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # apps
+    'ox_scale.apps.scale',
+
     # support
     'django_extensions',
 )
