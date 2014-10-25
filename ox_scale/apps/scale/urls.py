f273227a186a14e134fc794313a3a6691dd637ab
crccheck <c@crccheck.com>
2014-10-25 00:12:36 -0500

    make sure question views aren't cached
    so we can control what gets displayed every request

--- ox_scale/apps/scale/urls.py
+++ ox_scale/apps/scale/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_control
 
 from . import views
 
 
+random_question = views.RandomQuestion.as_view()
+
 urlpatterns = patterns('',
-    url(r'(?P<uuid>\w+)/$', views.RandomQuestion.as_view(),
+    url(r'(?P<uuid>\w+)/$', cache_control(max_age=0)(random_question),
         name='random_question')
 )
